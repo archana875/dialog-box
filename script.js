5b3fedd79979c4878c91af93dabe743588487ce4
archana875 <archangade478@gmail.com>
2024-11-05 23:46:31 -0800

    index file added

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModel(){
+    const containerElement = document.getElementById('container');
+    containerElement.style.display= 'flex';
+}
+
+function closeModel(){
+    const containerElement = document.getElementById('container');
+    containerElement.style.display= 'none';
+}
